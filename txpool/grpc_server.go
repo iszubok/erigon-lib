3fa50246895e353047f31e690b868c0d6c4ce709
leonardchinonso <leokingluthers@gmail.com>
2021-10-21 12:26:50 +0100

    debugging pending txs

--- txpool/grpc_server.go
+++ txpool/grpc_server.go
@@ -238,7 +238,7 @@ func (s *GrpcServer) Status(_ context.Context, _ *txpool_proto.StatusRequest) (*
 func (s *GrpcServer) GetTransactionCount(ctx context.Context, in *txpool_proto.TransactionCountRequest) (*txpool_proto.TransactionCountReply, error) {
 	addr := gointerfaces.ConvertH160toAddress(in.Address)
 	nonce, inPool := s.txPool.NonceFromAddress(addr)
-	fmt.Printf("Nonce and inPool: %v, %v\n", nonce, inPool)
+	log.Info("Nonce and inPool: \n", nonce, inPool)
 	return &txpool_proto.TransactionCountReply{
 		Nonce: nonce,
 		Found: inPool,
